a42df25d6b6e258f16e510170c97eaca93d63ee5
JiangGeGitHub <java_jiang_ge@163.com>
2019-06-11 14:59:06 +0800

    使用注释

--- src/main/java/com/example/demo/api/ApiCityController.java
+++ src/main/java/com/example/demo/api/ApiCityController.java
@@ -17,6 +17,12 @@ import java.util.List;
  * 访问地址 -http://127.0.0.1:8080/swagger-ui.html
  * 8080是项目端口号
  *
+ GET（SELECT）：从服务器取出资源（一项或多项）。
+ POST（CREATE）：在服务器新建一个资源。
+ PUT（UPDATE）：在服务器更新资源（客户端提供改变后的完整资源）。
+ PATCH（UPDATE）：在服务器更新资源（客户端提供改变的属性）。
+ DELETE（DELETE）：从服务器删除资源。
+
  200 OK - [GET]：服务器成功返回用户请求的数据，该操作是幂等的（Idempotent）。
  201 CREATED - [POST/PUT/PATCH]：用户新建或修改数据成功。
  202 Accepted - [*]：表示一个请求已经进入后台排队（异步任务）
