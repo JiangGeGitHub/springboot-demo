7b946e50f85269cc9fbf8bb9e7228b7c46da5c2d
JiangGeGitHub <java_jiang_ge@163.com>
2019-06-19 16:24:16 +0800

    集成vue

---
+++ src/main/java/com/example/demo/kafka/KafkaSender.java
@@ -0,0 +1,37 @@
+package com.example.demo.kafka;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class KafkaSender {
+    
+    private final org.slf4j.Logger log = LoggerFactory.getLogger(getClass());
+    
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private Gson gson = new GsonBuilder().create();
+
+    //发送消息方法
+    public void send() {
+        Message message = new Message();
+        message.setId(System.currentTimeMillis());
+        message.setMsg(UUID.randomUUID().toString());
+        message.setSendTime(new Date());
+        log.info("+++++++++++++++++++++  message = {}", gson.toJson(message));
+        try{
+            kafkaTemplate.send("zhisheng", gson.toJson(message));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import com.example.demo.kafka.KafkaSender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,10 +11,29 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Demo1ApplicationTests {
 
+	@Autowired
+	private KafkaSender sender;
+
 	@Test
-	public void contextLoads() {
+	public void kafkaTest() {
 
+		for (int i = 0; i < 3; i++) {
+			//调用消息发送类中的消息发送方法
+			sender.send();
 
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
+
+
+
+
+
+
+
